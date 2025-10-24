dfa2f4df82078fab07bdad688bfdd53fd69f6389
Syed Aashiq Ahmed <160868455+SyedAashiqAhmed@users.noreply.github.com>
2025-10-24 20:19:43 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3XU3hnTjU1waKY1A2OuYruEj7I9BIEW8",
+  apiKey: "YOUR-API-KEY",
   authDomain: "smart-path-navigator-43b4e.firebaseapp.com",
   databaseURL: "https://smart-path-navigator-43b4e-default-rtdb.firebaseio.com",
   projectId: "smart-path-navigator-43b4e",
@@ -15,3 +15,4 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
 export { database };
+
